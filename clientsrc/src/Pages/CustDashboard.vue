270e8e432b1157598c37e88df82e3c1d9a6ed1af
Nick Bynum <exander@live.com>
2020-05-01 17:07:57 -0600

    customer dashboard started

---
+++ clientsrc/src/Pages/CustDashboard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="custDashboard container-fluid">
+<div class="row text-center">
+  <div class="col-12 col-md-6">
+    <h1>There are {{NumOfProvidersAvail}} active in your area!</h1>
+  </div>
+  <div class="col-12 col-md-6 ">
+        <button
+          type="submit"
+          @click.prevent="postJob()"
+          class="btn btn-sm btn-success"
+        >Request Lawn Service</button>
+  </div>
+</div>
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'custDashboard',
+  data(){
+    return {}
+  },
+  computed:{},
+  methods:{},
+  components:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
