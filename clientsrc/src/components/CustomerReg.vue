5094f6c39cc92f6fa75eab50ec6a08ffbe5d7fa5
Preston Hess <hesspreston@outlook.com>
2020-05-01 14:33:24 -0700

    handoff

---
+++ clientsrc/src/components/CustomerReg.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="CustomerReg">
+    <div class="createNote">
+    <form>
+      <div class="form-group">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="First Name"
+          v-model="newCustomer.firstName"
+        />
+      </div>
+      <button
+        type="submit"
+        data-dismiss="modal"
+        @click="Register()"
+        class="btn btn-sm btn-dark"
+      >Register Now</button>
+    </form>
+  </div>
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'CustomerReg',
+  data(){
+    return {
+      newCustomer: {
+
+      }
+    }
+  },
+  computed:{},
+  methods:{},
+  components:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
