4d26aea2d9a2c07bc1dd871cb663cfbc12278589
Nick Bynum <exander@live.com>
2020-05-04 22:39:51 -0600

    to change computers

---
+++ clientsrc/src/components/UtilTable.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="UtilTable">
+<table class="table table-borderless">
+  <thead>
+    <tr>
+      <th scope="col">Address</th>
+      <th scope="col">Date and Time</th>
+      <th scope="col">Pice</th>
+      <th scope="col">Yard Size</th>
+    </tr>
+  </thead>
+  <tbody>
+    <slot></slot>
+  </tbody>
+</table>
+  </div>
+</template>
+
+
+<script>
+import custActiveTable from "../components/custActiveTable.vue"
+import custPrevTable from "../components/custPrevTable.vue"
+export default {
+  name: 'UtilTable',
+  data(){
+    return {}
+  },
+  computed:{},
+  methods:{},
+  components:{
+    'PendingJobs': custActiveTable,
+    'PrevJobs': custPrevTable,
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
