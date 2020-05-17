233011208763b01812896e1caf6c9d0eb14b4e8e
Nick Bynum <exander@live.com>
2020-05-16 23:39:22 -0600

    card layout updates

--- clientsrc/src/components/custCompleteTable.vue
+++ clientsrc/src/components/custCompleteTable.vue
@@ -3,19 +3,19 @@
   <div class="col-md-3 m-2">
     <div class="card">
       <div class="card-header bg-info">
-        <p class="text-white p-0 m-0 display-6">
+        <p class="text-white display-6">
           <strong>{{jobData.contactName}}</strong>
         </p>
       </div>
-      <div class="card-body text-capitalize text-info">
-        <p>{{jobData.streetAddress}}, {{jobData.city}}, {{jobData.state}} {{jobData.zipCode}}</p>
-        <p>Date: {{jobData.timeWindow}}</p>
-        <p>Job Price: ${{jobData.price}}</p>
-        <p>Yard Size: {{jobData.yardSize}}</p>
+      <div class="card-body text-capitalize text-danger text-left">
+        <p><strong>Addr: </strong>{{jobData.streetAddress}}, {{jobData.city}}, {{jobData.state}} {{jobData.zipCode}}</p>
+        <p><strong>Date: </strong>{{jobData.timeWindow}}</p>
+        <p><strong>Price: </strong>${{jobData.price}}</p>
+        <p><strong>Yard Size: </strong>{{jobData.yardSize}}</p>
       </div>
       <button
         @click="setJobForRating()"
-        class="btn btn-success btn-sm m-1 mt-3"
+        class="btn btn-success btn-sm m-1"
         data-toggle="modal"
         data-target="#jobRatingModal"
       >Leave feedback!</button>
@@ -45,4 +45,7 @@ export default {
 
 
 <style scoped>
+p{
+  margin: 0;
+}
 </style>
